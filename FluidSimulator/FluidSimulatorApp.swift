2e3ff69a8c170d1517d8dbe3dd8065a5a3409410
Agostin Jean-baptiste <jean-baptiste.agostin@abtasty.com>
2024-10-31 23:34:55 +0100

    Initial Commit

---
+++ FluidSimulator/FluidSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluidSimulatorApp.swift
+//  FluidSimulator
+//
+//  Created by Jean-Baptiste Agostin on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FluidSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FluidSimulatorTests/FluidSimulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluidSimulatorTests.swift
+//  FluidSimulatorTests
+//
+//  Created by Jean-Baptiste Agostin on 31/10/2024.
+//
+
+import Testing
+@testable import FluidSimulator
+
+struct FluidSimulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
